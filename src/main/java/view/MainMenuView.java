4a9bfb043953f46694d77e6fdf847bfd4998ac5a
Henry "TJ" Chen <127168121+LatinScribe@users.noreply.github.com>
2023-10-30 17:00:35 -0400

    Update to have the right buttons

--- src/main/java/view/MainMenuView.java
+++ src/main/java/view/MainMenuView.java
@@ -16,97 +16,106 @@ import java.beans.PropertyChangeListener;
 
 public class MainMenuView extends JPanel implements ActionListener, PropertyChangeListener {
 
-    public final String viewName = "log in";
-    private final interface_adapter.main_menu.MainMenuViewModel MainMenuViewModel;
+    public final String viewName = "Main Menu";
+    private final MainMenuViewModel mainMenuViewModel;
 
-    final JTextField usernameInputField = new JTextField(15);
-    private final JLabel usernameErrorField = new JLabel();
+//    final JTextField usernameInputField = new JTextField(15);
+//    private final JLabel usernameErrorField = new JLabel();
+//
+//    final JPasswordField passwordInputField = new JPasswordField(15);
+//    private final JLabel passwordErrorField = new JLabel();
 
-    final JPasswordField passwordInputField = new JPasswordField(15);
-    private final JLabel passwordErrorField = new JLabel();
+    final JButton signUp;
+    final JButton continueAsGuest;
 
-    final JButton logIn;
-    final JButton cancel;
+    final JButton signIn;
+
+    final JButton settings;
     private final MainMenuController mainMenuController;
 
-    public MainMenuView(MainMenuViewModel loginViewModel, MainMenuController controller) {
+    public MainMenuView(MainMenuViewModel mainMenuViewModel, MainMenuController controller) {
 
         this.mainMenuController = controller;
-        this.MainMenuViewModel = loginViewModel;
-        this.MainMenuViewModel.addPropertyChangeListener(this);
+        this.mainMenuViewModel = mainMenuViewModel;
+        this.mainMenuViewModel.addPropertyChangeListener(this);
 
-        JLabel title = new JLabel("Login Screen");
+        JLabel title = new JLabel("Main Menu Screen");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        LabelTextPanel usernameInfo = new LabelTextPanel(
-                new JLabel("Username"), usernameInputField);
-        LabelTextPanel passwordInfo = new LabelTextPanel(
-                new JLabel("Password"), passwordInputField);
+//        LabelTextPanel usernameInfo = new LabelTextPanel(
+//                new JLabel("Username"), usernameInputField);
+//        LabelTextPanel passwordInfo = new LabelTextPanel(
+//                new JLabel("Password"), passwordInputField);
 
         JPanel buttons = new JPanel();
-        logIn = new JButton(loginViewModel.LOGIN_BUTTON_LABEL);
-        buttons.add(logIn);
-        cancel = new JButton(loginViewModel.CANCEL_BUTTON_LABEL);
-        buttons.add(cancel);
+        // TODO: check mainMenuViewModel vs MainMenuViewModel
+        signUp = new JButton(MainMenuViewModel.SIGNUP_BUTTON_LABEL);
+        buttons.add(signUp);
+        continueAsGuest = new JButton(MainMenuViewModel.GUEST_BUTTON_LABEL);
+        buttons.add(continueAsGuest);
+        signIn = new JButton(MainMenuViewModel.SIGNIN_BUTTON_LABEL);
+        buttons.add(signIn);
+        settings = new JButton(MainMenuViewModel.SETTINGS_BUTTON_LABEL);
+        buttons.add(settings);
 
-        logIn.addActionListener(                // This creates an anonymous subclass of ActionListener and instantiates it.
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent evt) {
-                        if (evt.getSource().equals(logIn)) {
-                            MainMenuState currentState = loginViewModel.getState();
+//        logIn.addActionListener(                // This creates an anonymous subclass of ActionListener and instantiates it.
+//                new ActionListener() {
+//                    public void actionPerformed(ActionEvent evt) {
+//                        if (evt.getSource().equals(logIn)) {
+//                            MainMenuState currentState = mainMenuViewModel.getState();
+//
+//                            mainMenuController.execute(
+//                                    currentState.getUsername(),
+//                                    currentState.getPassword()
+//                            );
+//                        }
+//                    }
+//                }
+//        );
+//
+//        cancel.addActionListener(this);
 
-                            mainMenuController.execute(
-                                    currentState.getUsername(),
-                                    currentState.getPassword()
-                            );
-                        }
-                    }
-                }
-        );
-
-        cancel.addActionListener(this);
-
-        usernameInputField.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-                MainMenuState currentState = loginViewModel.getState();
-                currentState.setUsername(usernameInputField.getText() + e.getKeyChar());
-                loginViewModel.setState(currentState);
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-            }
-        });
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-        passwordInputField.addKeyListener(
-                new KeyListener() {
-                    @Override
-                    public void keyTyped(KeyEvent e) {
-                        MainMenuState currentState = loginViewModel.getState();
-                        currentState.setPassword(passwordInputField.getText() + e.getKeyChar());
-                        loginViewModel.setState(currentState);
-                    }
-
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                    }
-
-                    @Override
-                    public void keyReleased(KeyEvent e) {
-                    }
-                });
+//        usernameInputField.addKeyListener(new KeyListener() {
+//            @Override
+//            public void keyTyped(KeyEvent e) {
+//                MainMenuState currentState = mainMenuViewModel.getState();
+//                currentState.setUsername(usernameInputField.getText() + e.getKeyChar());
+//                mainMenuViewModel.setState(currentState);
+//            }
+//
+//            @Override
+//            public void keyPressed(KeyEvent e) {
+//            }
+//
+//            @Override
+//            public void keyReleased(KeyEvent e) {
+//            }
+//        });
+//        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+//
+//        passwordInputField.addKeyListener(
+//                new KeyListener() {
+//                    @Override
+//                    public void keyTyped(KeyEvent e) {
+//                        MainMenuState currentState = mainMenuViewModel.getState();
+//                        currentState.setPassword(passwordInputField.getText() + e.getKeyChar());
+//                        mainMenuViewModel.setState(currentState);
+//                    }
+//
+//                    @Override
+//                    public void keyPressed(KeyEvent e) {
+//                    }
+//
+//                    @Override
+//                    public void keyReleased(KeyEvent e) {
+//                    }
+//                });
 
         this.add(title);
-        this.add(usernameInfo);
-        this.add(usernameErrorField);
-        this.add(passwordInfo);
-        this.add(passwordErrorField);
+//        this.add(usernameInfo);
+//        this.add(usernameErrorField);
+//        this.add(passwordInfo);
+//        this.add(passwordErrorField);
         this.add(buttons);
     }
 
@@ -120,11 +129,11 @@ public class MainMenuView extends JPanel implements ActionListener, PropertyChan
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         MainMenuState state = (MainMenuState) evt.getNewValue();
-        setFields(state);
+        // setFields(state);
     }
 
-    private void setFields(MainMenuState state) {
-        usernameInputField.setText(state.getUsername());
-    }
+//    private void setFields(MainMenuState state) {
+//        usernameInputField.setText(state.getUsername());
+//    }
 
 }
