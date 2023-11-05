4772f006cda588bf6833e0e635785fed6c68797a
Henry TJ Chen <127168121+LatinScribe@users.noreply.github.com>
2023-11-04 21:30:00 -0400

    Create query by zip code

--- src/main/java/api/JsonCollegeScorecardDB.java
+++ src/main/java/api/JsonCollegeScorecardDB.java
@@ -63,10 +63,38 @@ public class JsonCollegeScorecardDB implements CollegeScorecardDB{
             throw new RuntimeException(e);
         }
     }
+    @Override
+    public JSONObject zipcodeQuery(String zipcode, String radius) {
+        // create the query url
+        String url = API_ADDRESS+ "schools.json?" + "zip=" + zipcode + "&" + "distance=" +radius + "&fields=id,school.name,2013.student.size"+ "&api_key=" + API_TOKEN;
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response);
+            JSONObject responseBody = new JSONObject(response.body().string());
+            return responseBody;
+
+//            if (responseBody.getInt("status_code") == 200) {
+//                JSONObject grade = responseBody.getJSONObject("grade");
+//                return grade;
+//            } else {
+//                throw new RuntimeException(responseBody.getString("message"));
+//            }
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void main(String[] args) {
         JsonCollegeScorecardDB db = new JsonCollegeScorecardDB();
         JSONObject stuff = db.basicQuery("school.degrees_awarded.predominant=2,3", "fields=id,school.name,2013.student.size");
         System.out.println(stuff);
+
+        JSONObject otherstuff = db.zipcodeQuery("02138", "5mi");
+        System.out.println(otherstuff);
     }
 }
