6ac6d4ad408e6f52e26e474e8d5bc50267b1e30f
Henry TJ Chen <127168121+LatinScribe@users.noreply.github.com>
2023-11-04 21:45:00 -0400

    Code clean up
    Created one helper function
    fixed types

--- src/main/java/api/JsonCollegeScorecardDB.java
+++ src/main/java/api/JsonCollegeScorecardDB.java
@@ -4,15 +4,16 @@ import entity.Grade;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 
-// This file contains the methods for accessing data through the College Score Card API
+// This file contains the methods for accessing data through the College Scorecard API
 // All methods return a JSON object
 // To get started, try running main and playing around with the basic query method
-// To proerly understand this file, highly reommended that you read the documentation found at CollegScorecardAccess.md
+// To properly understand this file, highly recommended that you read the documentation found at CollegScorecardAccess.md
 
 // Author: Henry
 // Version: 1.0
@@ -20,65 +21,10 @@ import java.io.IOException;
 public class JsonCollegeScorecardDB implements CollegeScorecardDB{
     String API_TOKEN = "AwypPp5bIHx6H4EevsB5sFs4gw5NK1M7SNc4eXwl";
     String API_ADDRESS = "https://api.data.gov/ed/collegescorecard/v1/";
-    @Override
-    public JSONObject basicQuery(String endpoint, String fileFormat, String queryParameters, String optionalParameters) {
-        // A more customisable basic query builder
-        // create the query url
-        String url = API_ADDRESS + endpoint + fileFormat + "?" + queryParameters + "&" + optionalParameters + "&api_key=" + API_TOKEN;
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        try {
-            Response response = client.newCall(request).execute();
-            System.out.println(response);
-            JSONObject responseBody = new JSONObject(response.body().string());
 
-            if (responseBody.getInt("status_code") == 200) {
-                JSONObject grade = responseBody.getJSONObject("grade");
-                return grade;
-            } else {
-                throw new RuntimeException(responseBody.getString("message"));
-            }
-        } catch (IOException | JSONException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    @Override
-    public JSONObject basicQuery(String queryParameters, String optionalParameters) {
-        // less customisable query, assumes you are querying for schools and returning a json
-        // recommend to use this
-        // create the query url
-        String url = API_ADDRESS+ "schools.json?" + queryParameters + "&" + optionalParameters + "&api_key=" + API_TOKEN;
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        try {
-            Response response = client.newCall(request).execute();
-            System.out.println(response);
-            JSONObject responseBody = new JSONObject(response.body().string());
-                return responseBody;
-
-//            if (responseBody.getInt("status_code") == 200) {
-//                JSONObject grade = responseBody.getJSONObject("grade");
-//                return grade;
-//            } else {
-//                throw new RuntimeException(responseBody.getString("message"));
-//            }
-        } catch (IOException | JSONException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    @Override
-    public JSONObject zipcodeQuery(String zipcode, String radius) {
-        // query a radius around a zip code for institutions
-        // NOTE: Make sure to add "mi" at the end of radius!
-        // create the query url
-        String url = API_ADDRESS+ "schools.json?" + "zip=" + zipcode + "&" + "distance=" +radius + "&fields=id,school.name,2013.student.size"+ "&api_key=" + API_TOKEN;
+    @NotNull
+    private JSONObject getJsonObject(String url) {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
@@ -99,6 +45,32 @@ public class JsonCollegeScorecardDB implements CollegeScorecardDB{
         } catch (IOException | JSONException e) {
             throw new RuntimeException(e);
         }
+    }
+    
+    @Override
+    public JSONObject basicQuery(String endpoint, String fileFormat, String queryParameters, String optionalParameters) {
+        // A more customisable basic query builder
+        // create the query url
+        String url = API_ADDRESS + endpoint + fileFormat + "?" + queryParameters + "&" + optionalParameters + "&api_key=" + API_TOKEN;
+        return getJsonObject(url);
+    }
+
+    @Override
+    public JSONObject basicQuery(String queryParameters, String optionalParameters) {
+        // less customisable query, assumes you are querying for schools and returning a json
+        // recommend to use this
+        // create the query url
+        String url = API_ADDRESS+ "schools.json?" + queryParameters + "&" + optionalParameters + "&api_key=" + API_TOKEN;
+        return getJsonObject(url);
+    }
+
+    @Override
+    public JSONObject zipcodeQuery(String zipcode, String radius) {
+        // query a radius around a zip code for institutions
+        // NOTE: Make sure to add "mi" at the end of radius!
+        // create the query url
+        String url = API_ADDRESS+ "schools.json?" + "zip=" + zipcode + "&" + "distance=" +radius + "&fields=id,school.name,2013.student.size"+ "&api_key=" + API_TOKEN;
+        return getJsonObject(url);
     }
 
     public static void main(String[] args) {
