4d075ff2e619b4cd2b58f12d5cc085f884302b5a
Diego <diego.pachasramos@mail.utoronto.ca>
2023-11-06 14:08:33 -0500

    Finished with the subview UI, implemented SubView.java.

--- src/main/java/view/SubView.java
+++ src/main/java/view/SubView.java
@@ -23,17 +23,39 @@ public class SubView extends JPanel implements ActionListener, PropertyChangeLis
     public SubView(SubViewModel subViewModel, SubViewController controller) {
         this.subcontroller = controller;
         this.subViewModel = subViewModel;
-        subViewModel.addPropertyChangeListener(this);
+        this.subViewModel.addPropertyChangeListener(this);
 
         JLabel title = new JLabel(SubViewModel.TITLE_LABEL);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
-
 
         JPanel buttons = new JPanel();
         recommendation = new JButton(SubViewModel.RECOMMENDATION_BUTTON_LABEL);
         buttons.add(recommendation);
         search = new JButton(SubViewModel.SEARCH_BUTTON_LABEL);
         buttons.add(search);
+
+        recommendation.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(recommendation)) {
+                            System.out.println("Recommendation Button pressed");
+                        }
+                    }
+                }
+        );
+
+        search.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(search)) {
+                            System.out.println("Search Button pressed");
+                        }
+                    }
+                }
+        );
+
+        this.add(title);
+        this.add(buttons);
     }
 
     public void actionPerformed(ActionEvent evt) {
@@ -41,7 +63,25 @@ public class SubView extends JPanel implements ActionListener, PropertyChangeLis
     }
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        // SubViewState state = (LoginState) evt.getNewValue();
+        SubViewState state = (SubViewState) evt.getNewValue();
         // setFields(state);
+    }
+
+    public static void main(String[] args) {
+        JFrame application = new JFrame("Sub menu Test");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        SubViewModel subViewModel = new SubViewModel();
+        SubViewController subViewController = new SubViewController();
+        SubView subView = new SubView(subViewModel, subViewController);
+
+        views.add(subView, subView.viewName);
+
+        application.pack();;
+        application.setVisible(true);
     }
 }
