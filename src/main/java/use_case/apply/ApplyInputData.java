515e714fae36d00b06e0253e5e8e7bf1a4bf1b5c
hbc03 <hboracelebi2003@gmail.com>
2023-11-13 16:35:49 -0500

    new apply stuff added

--- src/main/java/use_case/apply/ApplyInputData.java
+++ src/main/java/use_case/apply/ApplyInputData.java
@@ -1,4 +1,11 @@
 package use_case.apply;
 
 public class ApplyInputData {
+    final private String satScore;
+    final private String actScore;
+
+    public ApplyInputData(String satScore, String actScore) {
+        this.satScore = satScore;
+        this.actScore = actScore;
+    }
 }
