a2d7eb167cf590b85713ffdc0d79f6533de7db69
Henry TJ Chen <127168121+LatinScribe@users.noreply.github.com>
2023-11-18 00:33:19 -0500

    Created server data access object for profile
    - Created a DAO for user profiles to connect with our server
    - You can save, modify, and get profiles
    - NOTE: YOU MUST BE SIGNED IN TO USE THIS PROPERLY
    - Questions? ask on discord!
    - Also changed Userpreferences to use an arraylist of integers

--- src/main/java/entity/UserPreferencesFactory.java
+++ src/main/java/entity/UserPreferencesFactory.java
@@ -1,12 +1,13 @@
 package entity;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class UserPreferencesFactory {
 
     // Method to create a UserPreferences object with all fields
     public static UserPreferences createUserPreferences(Integer finAidRequirement, String preferredProgram,
-                                                        Integer avgSalary, Array universityRankingRange,
+                                                        Integer avgSalary, ArrayList<Integer> universityRankingRange,
                                                         String locationPreference) {
         return new UserPreferences(finAidRequirement, preferredProgram, avgSalary,
                 universityRankingRange, locationPreference);
