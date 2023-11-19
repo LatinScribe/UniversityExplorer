4cd02ff43208875d7eb360b913bfbf3cc140270a
hbc03 <hboracelebi2003@gmail.com>
2023-11-19 18:10:39 -0500

    new apply stuff added

--- src/main/java/data_access/ApplyDataAccessObject.java
+++ src/main/java/data_access/ApplyDataAccessObject.java
@@ -1,11 +1,14 @@
 package data_access;
 
+import api.JsonCollegeScorecardDB;
 import org.json.JSONObject;
 import use_case.apply.ApplyDataAccessInterface;
 
 public class ApplyDataAccessObject implements ApplyDataAccessInterface {
     @Override
     public JSONObject basicQuery(String queryParameters, String optionalParameters) {
-        return null;
+        JsonCollegeScorecardDB jsonCollegeScorecardDB = new JsonCollegeScorecardDB();
+        return jsonCollegeScorecardDB.basicQuery(queryParameters, optionalParameters);
+
     }
 }
--- src/main/java/interface_adapter/apply/ApplyController.java
+++ src/main/java/interface_adapter/apply/ApplyController.java
@@ -2,14 +2,17 @@
 package interface_adapter.apply;
 
 import use_case.apply.ApplyInputBoundary;
+import use_case.apply.ApplyInputData;
 
 public class ApplyController {
     final ApplyInputBoundary applyUseCaseInteractor;
-    public ApplyController(ApplyViewModel applyViewModel, ApplyInputBoundary applyUseCaseInteractor) {
+    public ApplyController( ApplyInputBoundary applyUseCaseInteractor) {
         this.applyUseCaseInteractor = applyUseCaseInteractor;
     }
 
-    public void execute(){
+    public void execute(String satScore , String actScore){
+        ApplyInputData applyInputData = new ApplyInputData(satScore,actScore);
+        applyUseCaseInteractor.execute(applyInputData);
 
     }
 }
