081c44510c8afb42a3a4e8ee2ee713157500025a
Henry TJ Chen <127168121+LatinScribe@users.noreply.github.com>
2023-11-25 23:32:54 -0500

    Create ServerUserDataAccessObjectTest.java

---
+++ src/main/test/data_access/ServerUserDataAccessObjectTest.java
@@ -0,0 +1,20 @@
+package data_access;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerUserDataAccessObjectTest {
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void existsByName() {
+    }
+
+    @Test
+    void save() {
+    }
+}
