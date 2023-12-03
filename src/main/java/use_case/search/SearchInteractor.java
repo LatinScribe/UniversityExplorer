267967412401bccd0e638303b6712aa2f4c7a6e6
André <andrec.lin@mail.utoronto.ca>
2023-12-02 23:16:02 -0500

    Fixing a quick applystate bug and some interactor cleanup

--- src/main/java/use_case/search/SearchInteractor.java
+++ src/main/java/use_case/search/SearchInteractor.java
@@ -11,7 +11,6 @@ import entity.UniversityFactory;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class SearchInteractor implements SearchInputBoundary {
     final SearchUserDataAccessInterface searchDataAccessObject;
